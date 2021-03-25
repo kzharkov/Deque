6531b9eb23eb513fc308d09a63a2d33b4747f395
sirkz <golang.freelance@gmail.com>
2021-03-26 01:39:39 +0700

    Добавьте файлы проекта.

---
+++ Source.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    class Deque<T>
+    {
+        LinkedList<T> list;
+        int size;
+
+        public Deque()
+        {
+            list = new LinkedList<T>();
+            size = 0;
+        }
+
+        public void AddFront(T item)
+        {
+            list.AddLast(item);
+            size++;
+        }
+
+        public void AddTail(T item)
+        {
+            list.AddFirst(item);
+            size++;
+        }
+
+        public T RemoveFront()
+        {
+            if (size == 0)
+            {
+                return default;
+            }
+            T item = list.Last.Value;
+            list.RemoveLast();
+            size--;
+            return item;
+        }
+
+        public T RemoveTail()
+        {
+            if (size == 0)
+            {
+                return default;
+            }
+            T item = list.First.Value;
+            list.RemoveFirst();
+            size--;
+            return item;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+    }
+
+}
